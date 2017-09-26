b9180ee9068830185f521d04f117f7cef3ce83bd
apple <mac@halls-10-40-146-5.wireless.leeds.ac.uk>
2017-09-26 17:33:06 +0100

    Initial Commit

---
+++ 20170926/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  20170926
+//
+//  Created by Zhonghai8705 on 2017/9/26.
+//  Copyright © 2017年 Zhonghai8705. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
