4e06ff527c4b6ca60ac761ea36ad381edc4b2fba
apple <mac@halls-10-40-148-55.wireless.leeds.ac.uk>
2017-11-23 01:37:21 +0000

    Buttons added to UI

---
+++ 2660project/2660 FinalProject/2660 FinalProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2660 FinalProject
+//
+//  Created by Zhonghai8705 on 2017/11/23.
+//  Copyright © 2017年 Zhonghai8705. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
